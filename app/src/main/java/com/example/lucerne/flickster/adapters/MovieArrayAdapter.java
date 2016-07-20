0e6b4aaa3ba2b122fee316115e5a206f9529cd9c
Lulu Li <lululi@g.harvard.edu>
2016-07-20 14:11:21 -0700

    Completing ViewHolder

---
+++ app/src/main/java/com/example/lucerne/flickster/adapters/MovieArrayAdapter.java
@@ -0,0 +1,72 @@
+package com.example.lucerne.flickster.adapters;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.lucerne.flickster.R;
+import com.example.lucerne.flickster.models.Movie;
+import com.squareup.picasso.Picasso;
+
+import java.util.List;
+
+/**
+ * Created by lucerne on 7/19/16.
+ */
+public class MovieArrayAdapter extends ArrayAdapter<Movie> {
+    // View lookup cache
+    private static class ViewHolder {
+        ImageView image;
+        TextView title;
+        TextView overview;
+    }
+
+    public MovieArrayAdapter(Context context, List<Movie> movies){
+        super(context, android.R.layout.simple_list_item_1, movies);
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        Movie movie = getItem(position);
+
+        ViewHolder viewHolder; // view lookup cache stored in tag
+
+        //Initiate container for inflate views from xml
+        if (convertView == null){
+            viewHolder = new ViewHolder();
+
+            LayoutInflater inflater = LayoutInflater.from(getContext());
+            convertView = inflater.inflate(R.layout.item_movie, parent, false);
+
+            viewHolder.image = (ImageView) convertView.findViewById(R.id.ivMovieImage);
+            viewHolder.title = (TextView) convertView.findViewById(R.id.tvTitle);
+            viewHolder.overview = (TextView) convertView.findViewById(R.id.tvOverview);
+            convertView.setTag(viewHolder);
+        } else {
+            viewHolder = (ViewHolder) convertView.getTag();
+        }
+
+        // Populate the data into the template view using the data object
+        viewHolder.image.setImageResource(0);
+        viewHolder.title.setText(movie.getOriginalTitle());
+        viewHolder.overview.setText(movie.getOverview());
+        Picasso.with(getContext()).load(movie.getPosterPath()).into(viewHolder.image);
+
+//        ImageView ivImage = (ImageView) convertView.findViewById(R.id.ivMovieImage);
+//        ivImage.setImageResource(0);
+//
+//        TextView tvTitle = (TextView) convertView.findViewById(R.id.tvTitle);
+//        TextView tvOverview = (TextView) convertView.findViewById(R.id.tvOverview);
+//
+//        tvTitle.setText(movie.getOriginalTitle());
+//        tvOverview.setText(movie.getOverview());
+//
+//        Picasso.with(getContext()).load(movie.getPosterPath()).into(ivImage);
+
+        return convertView;
+    }
+}
